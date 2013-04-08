2661173c4fd7bc4bd31884e3ab3b290aee578d56
vladiim <vladiim@yahoo.com.au>
2013-04-08 17:41:50 +1000

    Static plan & events working

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,12 +6,4 @@ function CategoryListCtrl($scope, $http) {
   $http.get('data/categories.json').success(function(data) {
     $scope.categories = data;
   });
-
-  // $scope.categories = [
-  //   { name: 'New Recruit', event_count: 430 },
-  //   { name: 'News', event_count: 1230 },
-  //   { name: 'Army', event_count: 759 },
-  //   { name: 'Air Force', event_count: 980 },
-  //   { name: 'Navy', event_count: 784 }
-  // ]
 }
