b019468b03e4be0f94b0454851fe1bf72ed9e397
vladiim <vladiim@yahoo.com.au>
2013-04-09 17:05:55 +1000

    Markdown can be edited

---
+++ app/data/markdown/test.md
@@ -0,0 +1,11 @@
+### Background
+
+
+
+### Strategy
+
+### Process
+
+1. Step 1
+2. Step 2
+3. Step 3
--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,39 @@
 'use strict';
 
-angular.module('Stacato', []);
+angular.module('Stacato', [])
+  .directive('markdown', function($http) {
+  	var converter = new Showdown.converter();
+  	var editTemplate = '<textarea id="bigdoc" ng-show="isEditMode" ng-dblclick="switchToPreview()" rows="20" ng-model="markdown"></textarea>';
+  	var previewTemplate = '<div ng-hide="isEditMode" ng-dblclick="switchToEdit()">Preview</div>';
+  	return {
+  		restrict: 'E',
+  		compile: function (tElement, tAttrs, transclude) {
+  			var markdown = tElement.text();
+  			// $http.get(tAttrs.src).then(function(data) {
+  			// 	return data.data;
+  			// });
 
-// angular.module('Stacato', ['myApp.filters', 'myApp.services', 'myApp.directives']).
-//   config(['$routeProvider', function($routeProvider) {
-//     $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
-//     $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
-//     $routeProvider.otherwise({redirectTo: '/view1'});
-//   }]);
+            tElement.html(editTemplate);
+            var previewElement = angular.element(previewTemplate);
+            tElement.append(previewElement);
+
+            return function (scope, element, attrs) {
+                scope.markdown = markdown;
+            	var makeHtml = converter.makeHtml(scope.markdown);
+                previewElement.html(makeHtml);
+
+            	scope.isEditMode = false;
+
+            	scope.switchToPreview = function() {
+            		scope.isEditMode = false;
+            	    var makeHtml = converter.makeHtml(scope.markdown);
+                    previewElement.html(makeHtml);
+            	}
+
+            	scope.switchToEdit = function() {
+            		scope.isEditMode = true;	
+            	}
+            }
+  		}
+  	}
+  });
